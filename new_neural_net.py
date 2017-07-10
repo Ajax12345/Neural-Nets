14ebb474b523de1358a127c7ab55d29e5e334b47
Ajax1234 <pentester18@gmail.com>
2017-07-10 16:17:04 -0400

    Update new_neural_net.py

--- new_neural_net.py
+++ new_neural_net.py
@@ -3,12 +3,14 @@ from random import randint
 
 class Neural:
     def __init__(self, x, y):
-        self.w = np.random.rand(3, 3)
+        #self.w = np.random.rand(3, 3)
         self.x = x
         self.y = y
+        #create w1 and w2
 
-    def phi(self, x):
-        return 1/float(1+np.exp(-x))
+
+    def phi(self, x): #passing vector
+        return 1/(1+np.exp(-x)) #carefull with float
 
     def gradient_descent(self):
         raise NotADirectoryError("Working on it")
@@ -27,15 +29,16 @@ if __name__ == "__main__":
     Y = [i[-1] for i in f]
 
 
-
+    #* linear algebra mult (numpy: a.dot(b))
+    #.* element-wise (numpy: a*b)
     newy = list(set(Y))
 
     many_to_one = {a:[1 if b == i else 0 for b in range(len(newy))] for i, a in enumerate(newy)}
+
+    print many_to_one
 
 
 
 
 
     yprime = [many_to_one[i] for i in Y]
-
-    print yprime
